5bb574ca3a222a1273098c7687d8cb5ef34ef396
Lisandro de souza walter <91380793+Lisandrowalter@users.noreply.github.com>
2022-08-16 22:29:23 -0300

    laravel

---
+++ laravel/resources/views/hotsite.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <title>INSTAPET</title>
+</head>
+<body>
+
+    
+    <div class="container">
+    <div class="container text-center">
+    <img src="gato.jpg" class="img-fluid " alt="..." height="400px">
+    <h1>INSTAPET</h1>
+    <h2>AME SEU PET</h2>   
+    </div>
+    <form method="post"action="validar.php" >
+    <div class="mb-3 text-center">
+        <label for="exampleInputEmail1" class="form-label">digite seu email</label>
+        <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">nós nunca compartinhamos seu email</div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+        <p>Venha fazer parte dessa familia pet seja menbro dessa grande familia</p>
+    </div> 
+
+    
+    
+    
+    </form>
+</div>
+    
+    
+</body>
+</html>
